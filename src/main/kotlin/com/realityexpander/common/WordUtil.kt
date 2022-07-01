0965f78622ed7cd0f7517da74b26bda84facf993
Chris Athanas <realityexpander@gmail.com>
2022-07-01 02:52:53 -0500

    Add word utils

---
+++ src/main/kotlin/com/realityexpander/common/WordUtil.kt
@@ -0,0 +1,51 @@
+package com.realityexpander.common
+
+import java.io.File
+
+val words = readWordList("src/main/resources/programmers_wordlist.txt")
+//val words = readWordList("resources/wordlist.txt") // alternate list
+
+fun readWordList(filePath: String): List<String> {
+    val file = File(filePath)
+    return file.readLines()
+}
+
+fun getRandomWord(): String {
+    val randomIndex = (0 until words.size).random()
+    return words[randomIndex]
+}
+
+fun getRandomWords(amount: Int): List<String> {
+    var curAmount = 0
+    val result = mutableListOf<String>()
+
+    while (curAmount < amount) {
+        val word = getRandomWord()
+        if (!result.contains(word)) {
+            result.add(word)
+            curAmount++
+        }
+    }
+
+    return result
+}
+
+
+// apple juice
+// -> _ _ _ _ _  _ _ _ _ _
+fun String.transformToUnderscores() = this.replace("[a-zA-Z]".toRegex(), "_ ")
+// in code:
+//toCharArray().map {
+//    if(it != ' ') '_' else ' ' }
+//.joinToString(" ")
+
+
+//FOR TESTING ONLY: val words = readWordList("src/main/resources/programmers_wordlist.txt")
+fun main() {
+    val word = getRandomWord()
+    println(word)
+    println(word.transformToUnderscores())
+
+    println("apple juice")
+    println("apple juice".transformToUnderscores())
+}
